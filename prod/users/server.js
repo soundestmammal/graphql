bc77743c0bc85ba679b5a08bbf40f37b4d47c14f
Robert Checco <robert.checco@gmail.com>
2019-06-05 15:03:29 -0400

    Use GraphQL in server.js (app.use)

--- prod/users/server.js
+++ prod/users/server.js
@@ -1,9 +1,15 @@
 // All logic for the express server
+// Convention is GraphQL "Uppercase G Q L"
+// graphiql is a dev tool
 
 const express = require('express');
+const expressGraphQL = require('express-graphql'); // glue between GraphQL and express
 
 const app = express();
-
+app.use('/graphql', expressGraphQL({
+    graphiql: true 
+}));
 app.listen(4000, () => {
     console.log("Listening on port 4000");
 });
+
